2865fa601f643fcddb1eb16f8fe09eeb986c2d2b
Red Ochsenbein <red@redochsenbein.ch>
2023-05-29 21:35:32 +0200

    First step: create title from url

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+#![deny(warnings)]
+
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use bytes::Bytes;
+use http_body_util::Full;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use tokio::net::TcpListener;
+
+// An async function that consumes a request, does nothing with it and returns a
+// response.
+async fn content(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    let title: String = req
+        .uri()
+        .path()
+        .to_string()
+        .replace("-", " ")
+        .chars()
+        .filter(|c| c.is_alphanumeric() || c.is_whitespace())
+        .collect();
+
+    let title = capitalize_words(&title);
+
+    // TODO fetch content from database
+    // TODO create links for each sentence
+    // TODO fetch content from ChatGPT if not found in database
+    // TODO store content in database
+
+    let body = format!(
+        r#"
+        This is a page about <strong>{}</strong>.
+        This is another line.
+        "#,
+        title
+    );
+
+    let html = format!(
+        r#"
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>{}</title>
+        </head>
+        <body>
+            <h1>{}</h1>
+            {}
+        </body>
+        </html>
+        "#,
+        title,
+        title,
+        body.trim().replace("\n", "<br>")
+    );
+
+    Ok(Response::new(Full::new(Bytes::from(html))))
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // This address is localhost
+    let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
+
+    // Bind to the port and listen for incoming TCP connections
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
+    loop {
+        // When an incoming TCP connection is received grab a TCP stream for
+        // client<->server communication.
+        //
+        // Note, this is a .await point, this loop will loop forever but is not a busy loop. The
+        // .await point allows the Tokio runtime to pull the task off of the thread until the task
+        // has work to do. In this case, a connection arrives on the port we are listening on and
+        // the task is woken up, at which point the task is then put back on a thread, and is
+        // driven forward by the runtime, eventually yielding a TCP stream.
+        let (stream, _) = listener.accept().await?;
+
+        // Spin up a new task in Tokio so we can continue to listen for new TCP connection on the
+        // current task without waiting for the processing of the HTTP1 connection we just received
+        // to finish
+        tokio::task::spawn(async move {
+            // Handle the connection from the client using HTTP1 and pass any
+            // HTTP requests received on that connection to the `content` function
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(stream, service_fn(content))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
+
+fn capitalize_words(s: &str) -> String {
+    let mut result = String::with_capacity(s.len());
+    let mut capitalize_next = true;
+    for c in s.chars() {
+        if c.is_whitespace() {
+            capitalize_next = true;
+            result.push(c);
+        } else if capitalize_next {
+            result.push(c.to_ascii_uppercase());
+            capitalize_next = false;
+        } else {
+            result.push(c.to_ascii_lowercase());
+        }
+    }
+    result
+}
